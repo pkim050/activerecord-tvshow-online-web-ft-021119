9997d916c84c1c956d194c426a71900f68a2039f
Patrick Kim <pkim050@gmail.com>
2019-03-26 16:20:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,29 +1,3 @@
 class Show < ActiveRecord::Base
-  def Show::highest_rating
-    Show.maxmium(:rating)
-  end
 
-  def most_popular_show
-
-  end
-
-  def lowest_rating
-
-  end
-
-  def least_popular_show
-
-  end
-
-  def ratings_sum
-
-  end
-
-  def popular_shows
-
-  end
-
-  def shows_by_alphabetical_order
-
-  end
 end
